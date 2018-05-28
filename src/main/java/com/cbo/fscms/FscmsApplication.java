724260f1ee10e559163f04972e86d185b7f4091b
cyrille.boucher@gmail.com <cyrille.boucher@gmail.com>
2018-05-28 21:10:58 +0200

    SRPING INIT
    As created by https://start.spring.io/

---
+++ src/main/java/com/cbo/fscms/FscmsApplication.java
@@ -0,0 +1,12 @@
+package com.cbo.fscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FscmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FscmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cbo/fscms/FscmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cbo.fscms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FscmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
